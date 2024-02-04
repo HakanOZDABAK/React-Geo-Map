47bd872852a87616bb7881dcbfd5f493eee1bd17
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-02-04 15:08:48 +0300

    some fix about map page

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useGeolocated  } from "react-geolocated";
-export default function Map(props: any) {
+export default function Map() {
     const geolocation = useGeolocated({
 positionOptions:{
-    enableHighAccuracy:true
-},userDecisionTimeout:1000
+    enableHighAccuracy:true,
+    maximumAge:0
+},userDecisionTimeout: 10000
       });
-    const DEFAULT_LANGITUDE = 35.3119022820198;
-    const DEFAULT_LATITUDE = 37.02601573209708;
+    const DEFAULT_LANGITUDE = 35.311508105246226;
+    const DEFAULT_LATITUDE = 37.03805979234759;
     const[longitude,setLongitude] = useState<any>(geolocation.coords ? geolocation.coords.longitude:DEFAULT_LANGITUDE)
     const[latitude,setLatitude] = useState<any>(geolocation.coords ? geolocation.coords.latitude:DEFAULT_LATITUDE)
 
@@ -16,7 +17,10 @@ positionOptions:{
         if (geolocation.coords) {
           setLongitude(geolocation.coords.longitude);
           setLatitude(geolocation.coords.latitude);
+
+         console.log(geolocation.coords.accuracy)
         }
+
       }, [geolocation.coords]);
      
     return (
