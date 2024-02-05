4325d026cc243eb82d51698e8ce76a4748839a0a
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-02-05 10:36:51 +0300

    added new map and double click to go to marker

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,8 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
 import { useGeolocated } from "react-geolocated";
 
+function LocationMarker({ setLocation }:{ setLocation :any }) {
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    dblclick(e) {
+      setLocation(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+    locationfound(e) {
+      setLocation(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return null;
+}
+
 export default function Map() {
+  const [location, setLocation] = useState(null);
   const geolocation = useGeolocated({
     positionOptions: {
       enableHighAccuracy: true,
@@ -10,6 +35,7 @@ export default function Map() {
     },
     userDecisionTimeout: 10000,
   });
+
   const DEFAULT_LANGITUDE = 17.63550145595826;
   const DEFAULT_LATITUDE = 35.242209769953156;
   const [longitude, setLongitude] = useState<any>(
@@ -23,20 +49,19 @@ export default function Map() {
     if (geolocation.coords) {
       setLongitude(geolocation.coords.longitude);
       setLatitude(geolocation.coords.latitude);
-
-      console.log(geolocation.coords.accuracy);
     }
   }, [geolocation.coords]);
 
   return (
     <MapContainer center={[latitude, longitude]} zoom={12}>
       <TileLayer
-        url=" ADDED NEW MAP"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org.copyright">OpenStreetMap</a> contributors'
-      ></TileLayer>
+      />
       <Marker position={[latitude, longitude]}>
         <Popup>Your are Here!</Popup>
       </Marker>
+      <LocationMarker setLocation={setLocation} />
     </MapContainer>
   );
 }
